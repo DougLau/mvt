08a258cc5fa079c8c85bd86b20eea3625e8fd053
Doug Lau <doug.p.lau@gmail.com>
2019-02-16 10:15:45 -0600

    Added more description and example to lib documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,37 @@
 //
 //! A library for encoding [mapbox vector tiles](https://github.com/mapbox/vector-tile-spec)
 //! (MVT).
+//!
+//! A [tile](struct.Tile.html) is composed of one or more
+//! [layer](struct.Layer.html)s.  Each layer can have any number of
+//! [feature](struct.Feature.html)s, which contain the geometry to be rendered.
+//! They can also have metadata tags, which are key/value pairs.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use mvt::{Error, GeomEncoder, GeomType, Tile, Transform};
+//!
+//! fn main() -> Result<(), Error> {
+//!     let mut tile = Tile::new(4096);
+//!     let layer = tile.create_layer("First Layer");
+//!     let b = GeomEncoder::new(GeomType::Linestring, Transform::new())
+//!                         .point(0.0, 0.0)
+//!                         .point(1024.0, 0.0)
+//!                         .point(1024.0, 2048.0)
+//!                         .point(2048.0, 2048.0)
+//!                         .point(2048.0, 4096.0)
+//!                         .encode()?;
+//!     let mut feature = layer.into_feature(b);
+//!     feature.set_id(1);
+//!     feature.add_tag_string("key", "value");
+//!     let layer = feature.into_layer();
+//!     tile.add_layer(layer)?;
+//!     let data = tile.to_bytes()?;
+//!     println!("encoded {} bytes: {:?}", data.len(), data);
+//!     Ok(())
+//! }
+//! ```
 #[macro_use] extern crate log;
 
 mod encoder;
